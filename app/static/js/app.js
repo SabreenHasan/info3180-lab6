c83c53dd77c7b0e52682eed32ae475a838b8fa19
Sabreen Hasan <sabreen_hasan@yahoo.com>
2020-04-16 17:05:23 -0500

    create component

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,4 +1,20 @@
 /* Add your Application JavaScript */
+Vue.component('news-list', {
+    template: `
+        <div class="news">
+            <h2>News</h2>
+            <ul class="news__list">
+                <li class="news__item">News item 1</li>
+                <li class="news__item">News item 2</li>
+                <li class="news__item">News item 3</li>
+            </ul>
+        </div>
+    `,
+    data: function() {
+      return {};
+    }
+});
+
 Vue.component('app-header', {
     template: `
         <header>
