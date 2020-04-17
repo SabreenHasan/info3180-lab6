9946cb2d5fdd652e82bf3ff01a2fb2e60ae27ae9
Sabreen Hasan <sabreen_hasan@yahoo.com>
2020-04-17 00:40:29 -0500

    fetched data listed

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -4,9 +4,8 @@ Vue.component('news-list', {
         <div class="news">
             <h2>News</h2>
             <ul class="news__list">
-                <li class="news__item">News item 1</li>
-                <li class="news__item">News item 2</li>
-                <li class="news__item">News item 3</li>
+                <li v-for="article in articles"
+                class="news__item">{{ article.title }}</li>
             </ul>
         </div>
     `,
