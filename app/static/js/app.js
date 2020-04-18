b1319f9f45dcd0c9d32e989f633120f8bd74cf28
Sabreen Hasan <sabreen_hasan@yahoo.com>
2020-04-17 20:54:38 -0500

    search bar, data binding and event handling added

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,31 @@
 /* Add your Application JavaScript */
-Vue.component('news-list', {
+const Home = Vue.component('home', {
+    template: `
+        <div class="home">
+            <img src="/static/images/logo.png" alt="VueJS Logo">
+            <h1>{{ welcome }}</h1>
+        </div>
+    `,
+    data: function() {
+        return {
+            welcome: 'Hello World! Welcome to VueJS'
+        }
+    }
+});
+
+const NewsList = Vue.component('news-list', {
     template: `
         <div class="news">
             <h2>News</h2>
             <div id="newsMainGrid">
+                <div class="form-inline d-flex justify-content-center" id="searchGrid">
+                    <div class="form-group mx-sm-3 mb-2">
+                        <label class="sr-only" for="search">Search</label>
+                        <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+                        <button class="btn btn-primary mb-2"@click="searchNews">Search</button>
+                        <p>You are searching for {{ searchTerm }}</p>
+                    </div>
+                </div>
                 <div v-for="article in articles"
                     class="news__item"
                     id="newsBox">{{ article.title }}<img :src=article.urlToImage></img>{{ article.description }}</div>                                    
@@ -23,10 +45,25 @@ Vue.component('news-list', {
             });
     },
     data: function() {
-      return {
-          articles: []
-      };
-    }
+        return {
+            articles: [],
+            searchTerm: ''
+        };
+    },
+    methods: {
+        searchNews: function() {
+            let self = this;
+            
+            fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + '&language=en&apiKey=<your-api-key>')
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log(data);
+                    self.articles = data.articles;
+                });
+            }
+        }
 });
 
 Vue.component('app-header', {
@@ -41,10 +78,10 @@ Vue.component('app-header', {
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                   <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <router-link to="/" class="nav-link">Home</router-link>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">News</a>
+                  <li class="nav-item active">
+                    <router-link to="/news" class="nav-link">News</router-link>
                   </li>
                 </ul>
               </div>
@@ -71,11 +108,16 @@ Vue.component('app-footer', {
     }
 })
 
-
-let app = new Vue({
-    el: '#app',
-    data: {
-        welcome: 'Hello World! Welcome to VueJS'
-    }
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        { path: '/', component: Home },
+        { path: '/news', component: NewsList }
+    ]
 });
+
+const app = new Vue({
+    el: '#app',
+    router
+})
 
